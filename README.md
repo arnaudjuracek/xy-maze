34c83e79a8142021a04cca9626931be90af5049f
RNO <arnaud.juracek@gmail.com>
2016-09-26 01:01:03 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+<h1 align="center">xy-maze</h1>
+<div align="center">
+  <!-- License -->
+  <a href="https://raw.githubusercontent.com/arnaudjuracek/xy/master/LICENSE">
+    <img src="https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square" alt="License" />
+  </a>
+</div>
+===
+
+![preview](maze.png?raw=true "preview")
+
+### References
+
+- [arnaudjuracek/maze-generator](https://github.com/arnaudjuracek/maze-generator)
+- [arnaudjuracek/xy](https://github.com/arnaudjuracek/xy)
+
+### Installation
+```sh
+npm install --save arnaudjuracek/xy-maze
+```
+
+### License
+MIT.
---
+++ index.js
@@ -0,0 +1,97 @@
+const plotter = require('xy-plotter');
+const mg      = require('maze-generator');
+
+
+const
+  cols = 100,
+  rows = 100,
+  margin = 10,
+  scale = (plotter.width - margin * 2) / cols,
+  maze = new mg.Maze(cols, rows, 0, 0).generate(),
+  solver = new mg.Solver(maze).solve(),
+  job = plotter.Job('maze');
+
+let walls = {
+  vertical: [],
+  horizontal: [],
+};
+
+for (let col = 0; col < cols; col++) {
+  let verticalWalls = (col === cols - 1) ? [[], []] : [];
+  for (let row = 0; row < rows; row++) {
+    let index = col + row * rows;
+    let cell = maze.cells[index];
+    for (let j = 0; j < cell.walls.length; j++) {
+      let wall = cell.walls[j];
+      if (col === cols - 1) {
+        if (j === 3) verticalWalls[0].push(wall);
+        if (j === 1) verticalWalls[1].push(wall);
+      } else {
+        if (j === 3) verticalWalls.push(wall);
+      }
+    }
+  }
+  if (col === cols - 1) {
+    walls.vertical.push(verticalWalls[0]);
+    walls.vertical.push(verticalWalls[1]);
+  } else {
+    walls.vertical.push(verticalWalls);
+  }
+}
+
+for (let row = 0; row < rows; row++) {
+  let horizontalWalls = (row === rows - 1) ? [[], []] : [];
+  for (let col = 0; col < cols; col++) {
+    let index = col + row * rows;
+    let cell = maze.cells[index];
+    for (let j = 0; j < cell.walls.length; j++) {
+      let wall = cell.walls[j];
+      if (row === rows - 1) {
+        if (j === 0) horizontalWalls[0].push(wall);
+        if (j === 2) horizontalWalls[1].push(wall);
+      } else {
+        if (j === 0) horizontalWalls.push(wall);
+      }
+    }
+  }
+  if (row === rows - 1) {
+    walls.horizontal.push(horizontalWalls[0]);
+    walls.horizontal.push(horizontalWalls[1]);
+  } else {
+    walls.horizontal.push(horizontalWalls);
+  }
+}
+
+job.circle(margin + 0 * scale + scale / 2, margin + 0 * scale + scale / 2, scale * 0.25, 5);
+job.circle(margin + maze.end.x * scale + scale / 2, margin + maze.end.y * scale + scale / 2, scale * 0.25, 5);
+
+job.setSpeed(0.8);
+for (let col = 0; col < walls.vertical.length; col++) {
+  let segments = walls.vertical[col];
+  job.pen_up();
+  for (let i = 0; i < segments.length; i++) {
+    let segment = segments[i];
+    job.move(margin + col * scale, margin + i * scale);
+    if (segment) job.pen_down();
+    else job.pen_up();
+  }
+  job.move(margin + col * scale, margin + segments.length * scale);
+}
+
+for (let row = 0; row < walls.horizontal.length; row++) {
+  let segments = walls.horizontal[row];
+  job.pen_up();
+  for (let i = 0; i < segments.length; i++) {
+    let segment = segments[i];
+    job.move(margin + i * scale, margin + row * scale);
+    if (segment) job.pen_down();
+    else job.pen_up();
+  }
+  job.move(margin + segments.length * scale, margin + row * scale);
+}
+
+const path = require('path');
+const file = plotter.File();
+file.write(job, path.join(__dirname, 'maze.png'));
+
+// plotter.Serial('/dev/tty.wchusbserial1410').send(job);
